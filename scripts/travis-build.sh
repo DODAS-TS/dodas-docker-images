f1a5bcb4454c2c583f5acf8ca77c53e44402c8b3
dciangot <diego.ciangottini@gmail.com>
2020-03-08 15:36:34 +0100

    fix if in build

--- scripts/travis-build.sh
+++ scripts/travis-build.sh
@@ -4,7 +4,4 @@ echo "git diff-tree --no-commit-id --name-only -r HEAD~1 | grep $2"
 
 CH_FILES=`git diff-tree --no-commit-id --name-only -r HEAD~1 | grep $2`
 
-if ! docker pull $1; then
-    docker build -t $1 $2
-fi
-
+docker build -t $1 $2
