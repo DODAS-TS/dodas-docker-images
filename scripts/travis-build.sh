b808c63e9eddb42f34b177869a9abe4436d1d062
dciangot <diego.ciangottini@gmail.com>
2020-03-09 10:34:41 +0100

    for now build everything

--- scripts/travis-build.sh
+++ scripts/travis-build.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-echo "git diff-tree --no-commit-id --name-only -r HEAD~1 | grep $2"
+#echo "git diff-tree --no-commit-id --name-only -r HEAD~1 | grep $2"
 
-git diff-tree --no-commit-id --name-only -r HEAD~1 | grep -c No $2 
-
-if [ $? -eq 0 ]; then
-    docker build -t $1 $2
-fi
+docker build -t $1 $2
