ea95275dec187f1897ab6a5e458116d81b894737
dciangot <diego.ciangottini@gmail.com>
2020-03-10 16:12:13 +0100

    apply patch for scitokens

--- docker/cachingondemand/xrootd-escape-http/scripts/start.sh
+++ docker/cachingondemand/xrootd-escape-http/scripts/start.sh
@@ -5,6 +5,7 @@ if [ ! -f /etc/grid-security/hostcert.pem ]; then
     chown -R xrootd: /etc/grid-security/hostcert.pem /etc/grid-security/hostcert.key
     for i in `openssl x509 -in /etc/grid-security/certificates/DODAS.pem -subject_hash`; do
         ln -s /etc/grid-security/certificates/DODAS.pem /etc/grid-security/certificates/$i.0
+        chown -R xrootd: /etc/grid-security/hostcert.pem /etc/grid-security/hostcert.key
         break
     done
 fi
