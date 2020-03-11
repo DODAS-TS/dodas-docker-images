0e298ef720a489b224f47f0d26a77178452f20a6
dciangot <diego.ciangottini@gmail.com>
2020-03-11 17:07:31 +0100

    compose for cache http

---
+++ docker/cachingondemand/xcache-escape-http/scripts/start.sh
@@ -0,0 +1,25 @@
+#! /bin/bash
+
+if [ ! -f /etc/grid-security/certificates/DODAS.pem ]; then
+
+    /usr/local/bin/dodas-x509 --hostname $XRD_HOST --gen-certs --ca-path /etc/grid-security/certificates --cert-path /etc/grid-security --ca-name DODAS
+    for i in `openssl x509 -in /etc/grid-security/certificates/DODAS.pem -subject_hash`; do
+        ln -s /etc/grid-security/certificates/DODAS.pem /etc/grid-security/certificates/$i.0
+        break
+    done
+
+fi
+
+if [ ! -f /etc/grid-security/hostcert.pem ]; then
+    /usr/local/bin/dodas-x509 --hostname $XRD_HOST --ca-path /etc/grid-security/certificates --cert-path /etc/grid-security --ca-name DODAS
+    chown -R xrootd: /etc/grid-security/hostcert.pem /etc/grid-security/hostcert.key
+    for i in `openssl x509 -in /etc/grid-security/certificates/DODAS.pem -subject_hash`; do
+        ln -s /etc/grid-security/certificates/DODAS.pem /etc/grid-security/certificates/$i.0
+        chown -R xrootd: /etc/grid-security/hostcert.pem /etc/grid-security/hostcert.key
+        break
+    done
+fi
+
+sudo -E -u xrootd /usr/bin/xrootd -k 3 -l /var/log/xrootd/xrootd.log -c /etc/xrootd/xrootd-http.cfg -n http &
+
+sleep infinity
