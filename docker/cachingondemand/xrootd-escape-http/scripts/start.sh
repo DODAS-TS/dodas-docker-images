1bac20aded147198d8580527dacfaedbcff1ea73
dciangot <diego.ciangottini@gmail.com>
2020-03-16 17:58:23 +0100

    update http example

--- docker/cachingondemand/xrootd-escape-http/scripts/start.sh
+++ docker/cachingondemand/xrootd-escape-http/scripts/start.sh
@@ -1,10 +1,12 @@
 #! /bin/bash
 
-if [ ! -f /etc/grid-security/certificates/DODAS.pem ]; then
+if [ -f /etc/grid-security/certificates/DODAS.pem ]; then
 
+    echo "Using predefined CA"
     /usr/local/bin/dodas-x509 --hostname $XRD_HOST --generate-cert --ca-path /etc/grid-security/certificates --cert-path /etc/grid-security --ca-name DODAS
     for i in `openssl x509 -in /etc/grid-security/certificates/DODAS.pem -subject_hash`; do
         ln -s /etc/grid-security/certificates/DODAS.pem /etc/grid-security/certificates/$i.0
+        chown -R xrootd: /etc/grid-security/hostcert.pem /etc/grid-security/hostcert.key
         break
     done
 
