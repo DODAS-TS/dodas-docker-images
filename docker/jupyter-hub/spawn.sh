4278408740fc6683c697dd39030daeef6b64da69
dciangot <diego.ciangottini@gmail.com>
2020-09-23 08:57:32 +0200

    cleanup jupyterhub and update spark jar

--- docker/jupyter-hub/spawn.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-if ![ -z $S3_ENABLED ]; then
-    mkdir -p s3/${USERNAME} && cd .init/ && ./sts-wire ${USERNAME} ${S3_HOST} /${USERNAME} ../s3/${USERNAME} > .mount_log_${USERNAME}.txt &
-else
-    echo "S3 disable."
-fi
