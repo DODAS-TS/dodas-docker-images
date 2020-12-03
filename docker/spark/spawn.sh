9c405b787e53129ec3d7a606cbf8779f66cf75ca
marcoverl <marco.verlato@pd.infn.it>
2020-12-03 22:45:29 +0100

    Update spawn.sh

--- docker/spark/spawn.sh
+++ docker/spark/spawn.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-if ! [ -z $S3_ENABLED ]; then
-    if ! [ -z $S3_BUCKET ]; then
-      mkdir -p s3/${S3_BUCKET} && cd .init/ && ./sts-wire myminio ${S3_ENDPOINT} /${S3_BUCKET} ../s3/${S3_BUCKET} > .mount_log_${S3_BUCKET}.txt &
-    fi
+if [ $S3_BUCKET ] && [ $S3_ENDPOINT ]; then
+    mkdir -p s3/${S3_BUCKET} && cd .init/ && ./sts-wire myminio ${S3_ENDPOINT} /${S3_BUCKET} ../s3/${S3_BUCKET} > .mount_log_${S3_BUCKET}.txt &
 else
-    echo "S3 disable."
+    echo "S3 disabled."
 fi
