fb984d01efd86161dc2d747ac9353066f2759c9e
marcoverl <marco.verlato@pd.infn.it>
2020-12-16 16:29:43 +0100

    allow for more s3 buckets

--- docker/spark/spawn.sh
+++ docker/spark/spawn.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 if [ ${S3_BUCKETS%% *} ] && [ $S3_ENDPOINT ]; then
-    for S3_BUCKET in ${S3_BUCKETS}; do mkdir -p s3 && cd .init && ./sts-wire ${S3_BUCKET} ${S3_ENDPOINT} /${S3_BUCKET} ../s3/${S3_BUCKET} > .mount_log_${S3_BUCKET}.txt & && cd ..; done
+    mkdir -p s3
+    for S3_BUCKET in ${S3_BUCKETS};
+    do
+            .init/sts-wire ${S3_BUCKET} ${S3_ENDPOINT} /${S3_BUCKET} s3/${S3_BUCKET} > .mount_log_${S3_BUCKET}.txt &
+    done
 else
     echo "S3 disabled."
 fi
