38958081bc99664821ef4ad3ec366417893ed665
marcoverl <marco.verlato@pd.infn.it>
2021-01-19 16:23:22 +0100

    Update spawn.sh
    using new sts-wire release

--- docker/spark/spawn.sh
+++ docker/spark/spawn.sh
@@ -3,7 +3,8 @@ if [ ${S3_BUCKETS%% *} ] && [ $S3_ENDPOINT ]; then
     mkdir -p s3
     for S3_BUCKET in ${S3_BUCKETS};
     do
-            .init/sts-wire ${S3_BUCKET} ${S3_ENDPOINT} /${S3_BUCKET} s3/${S3_BUCKET} > .mount_log_${S3_BUCKET}.txt &
+            .init/sts-wire $IAM_SERVER ${S3_BUCKET} ${S3_ENDPOINT} /${S3_BUCKET} s3/${S3_BUCKET} > .mount_log_${S3_BUCKET}.txt &
+	        sleep 10
     done
 else
     echo "S3 disabled."
