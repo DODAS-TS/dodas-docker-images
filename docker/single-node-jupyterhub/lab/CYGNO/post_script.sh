b34c1934a0aac7b7f9098614b4b061d6c61d9898
MircoT <MircoT@users.noreply.github.com>
2021-11-02 15:15:57 +0100

    Update post script on cygno

--- docker/single-node-jupyterhub/lab/CYGNO/post_script.sh
+++ docker/single-node-jupyterhub/lab/CYGNO/post_script.sh
@@ -1,11 +1,56 @@
 #!/usr/bin/env bash
 
+BASE_CACHE_DIR="/usr/local/share/dodasts/sts-wire/cache"
+COMMON_PARAMS=" --localCache full --tryRemount"
+
+mkdir -p "${BASE_CACHE_DIR}"
+mkdir -p /usr/local/share/dodasts/sts-wire/cache
 mkdir -p /var/log/sts-wire/
 mkdir -p /s3/
 mkdir -p /s3/"${USERNAME}"
 mkdir -p /s3/scratch
-mkdir -p /s3/cygnus
+mkdir -p /s3/cygno
+mkdir -p /s3/cygno-analysis
+mkdir -p /s3/cygno-sim
+mkdir -p /s3/cygno-data
 
-sts-wire https://iam.cloud.infn.it/ "${USERNAME}" https://minio.cloud.infn.it/ "/${USERNAME}" "/s3/${USERNAME}" &>"/var/log/sts-wire/mount_log_${USERNAME}.log" &
-sts-wire https://iam.cloud.infn.it/ scratch https://minio.cloud.infn.it/ /scratch /s3/scratch &>/var/log/sts-wire/mount_log_scratch.log &
-sts-wire https://iam.cloud.infn.it/ cygnus https://minio.cloud.infn.it/ /cygnus /s3/cygnus &>/var/log/sts-wire/mount_log_cygnus.log &
+# sts-wire https://iam.cloud.infn.it/ "${USERNAME}" https://minio.cloud.infn.it/ "/${USERNAME}" "/s3/${USERNAME}" &>"/var/log/sts-wire/mount_log_${USERNAME}.log" &
+# sts-wire https://iam.cloud.infn.it/ scratch https://minio.cloud.infn.it/ /scratch /s3/scratch &>/var/log/sts-wire/mount_log_scratch.log &
+# sts-wire https://iam.cloud.infn.it/ cygnus https://minio.cloud.infn.it/ /cygnus /s3/cygnus &>/var/log/sts-wire/mount_log_cygnus.log &
+
+sleep 5s && sts-wire https://iam.cloud.infn.it/ \
+    "${USERNAME}" https://minio.cloud.infn.it/ \
+    "/${USERNAME}" "/s3/${USERNAME}" \
+    ${COMMON_PARAMS} \
+    --localCacheDir "${BASE_CACHE_DIR}/${USERNAME}" \
+    &>"/var/log/sts-wire/mount_log_${USERNAME}.txt" &
+sleep 5s && sts-wire https://iam.cloud.infn.it/ \
+    scratch https://minio.cloud.infn.it/ \
+    /scratch /s3/scratch \
+    ${COMMON_PARAMS} \
+    --localCacheDir "${BASE_CACHE_DIR}/scratch" \
+    &>/var/log/sts-wire/mount_log_scratch.txt &
+sleep 5s && sts-wire https://iam.cloud.infn.it/ \
+    cygno https://minio.cloud.infn.it/ \
+    /cygnus /s3/cygno \
+    ${COMMON_PARAMS} \
+    --localCacheDir "${BASE_CACHE_DIR}/cygno" \
+    &>/var/log/sts-wire/mount_log_cygno.txt &
+sleep 5s && sts-wire https://iam.cloud.infn.it/ \
+    cygno_analysis https://minio.cloud.infn.it/ \
+    /cygno-analysis /s3/cygno-analysis \
+    ${COMMON_PARAMS} \
+    --localCacheDir "${BASE_CACHE_DIR}/cygno_analysis" \
+    &>/var/log/sts-wire/mount_log_cygnoalanysis.txt &
+sleep 5s && sts-wire https://iam.cloud.infn.it/ \
+    cygno_sim https://minio.cloud.infn.it/ \
+    /cygno-sim /s3/cygno-sim \
+    ${COMMON_PARAMS} \
+    --localCacheDir "${BASE_CACHE_DIR}/cygno_sim" \
+    &>/var/log/sts-wire/mount_log_cygnosim.txt &
+sleep 5s && sts-wire https://iam.cloud.infn.it/ \
+    cygno_data https://minio.cloud.infn.it/ \
+    /cygno-data /s3/cygno-data \
+    ${COMMON_PARAMS} \
+    --localCacheDir "${BASE_CACHE_DIR}/cygno_data" \
+    &>/var/log/sts-wire/mount_log_cygnodata.txt &
