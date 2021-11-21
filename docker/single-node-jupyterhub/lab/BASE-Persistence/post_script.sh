3d290514eb6fa4c78b510e4df4ce2a2277300c3d
spiga <daniele.spiga@pg.infn.it>
2021-11-21 20:53:00 +0100

    add image for jupyter with persistence

---
+++ docker/single-node-jupyterhub/lab/BASE-Persistence/post_script.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+BASE_CACHE_DIR="/usr/local/share/dodasts/sts-wire/cache"
+COMMON_PARAMS=" --localCache full --tryRemount --noDummyFileCheck"
+
+mkdir -p "${BASE_CACHE_DIR}"
+mkdir -p /usr/local/share/dodasts/sts-wire/cache
+mkdir -p /var/log/sts-wire/
+mkdir -p /s3/
+mkdir -p /s3/"${USERNAME}"
+mkdir -p /s3/scratch
+
+sleep 1s && nice -n 19 sts-wire https://iam.cloud.infn.it/ \
+    "${USERNAME}" https://minio.cloud.infn.it/ \
+    "/${USERNAME}" "/s3/${USERNAME}" \
+    ${COMMON_PARAMS} \
+    --localCacheDir "${BASE_CACHE_DIR}/${USERNAME}" \
+    &>"/var/log/sts-wire/mount_log_${USERNAME}.txt" &
+sleep 2s && nice -n 19 sts-wire https://iam.cloud.infn.it/ \
+    scratch https://minio.cloud.infn.it/ \
+    /scratch /s3/scratch \
+    ${COMMON_PARAMS} \
+    --localCacheDir "${BASE_CACHE_DIR}/scratch" \
+    &>/var/log/sts-wire/mount_log_scratch.txt &
