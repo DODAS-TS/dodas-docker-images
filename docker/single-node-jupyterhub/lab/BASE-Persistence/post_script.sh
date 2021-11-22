3238c3409489058ea15d8f29bc6da8f645d7822d
MircoT <MircoT@users.noreply.github.com>
2021-11-22 09:07:56 +0100

    Fix SC2086

--- docker/single-node-jupyterhub/lab/BASE-Persistence/post_script.sh
+++ docker/single-node-jupyterhub/lab/BASE-Persistence/post_script.sh
@@ -13,12 +13,12 @@ mkdir -p /s3/scratch
 sleep 1s && nice -n 19 sts-wire https://iam.cloud.infn.it/ \
     "${USERNAME}" https://minio.cloud.infn.it/ \
     "/${USERNAME}" "/s3/${USERNAME}" \
-    ${COMMON_PARAMS} \
+    "${COMMON_PARAMS}" \
     --localCacheDir "${BASE_CACHE_DIR}/${USERNAME}" \
     &>"/var/log/sts-wire/mount_log_${USERNAME}.txt" &
 sleep 2s && nice -n 19 sts-wire https://iam.cloud.infn.it/ \
     scratch https://minio.cloud.infn.it/ \
     /scratch /s3/scratch \
-    ${COMMON_PARAMS} \
+    "${COMMON_PARAMS}" \
     --localCacheDir "${BASE_CACHE_DIR}/scratch" \
     &>/var/log/sts-wire/mount_log_scratch.txt &
