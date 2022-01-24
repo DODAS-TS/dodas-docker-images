c89f00664b52b7aef09607932159cb6dffa08661
MircoT <MircoT@users.noreply.github.com>
2022-01-24 16:44:21 +0100

    Fix install scripts and folders

--- docker/CYGNO/scripts/install_garfield.sh
+++ docker/CYGNO/scripts/install_garfield.sh
@@ -1,11 +1,18 @@
 #!/usr/bin/env bash
 
-git clone https://gitlab.cern.ch/garfield/garfieldpp.git "${GARFIELD_HOME}" && mkdir build
+mkdir -p "${GARFIELD_HOME}"
 
-pushd /usr/local/share/garfield/build || exit 1
+pushd "${GARFIELD_HOME}" || exit 1
+
+git clone https://gitlab.cern.ch/garfield/garfieldpp.git "${GARFIELD_HOME}" &&
+    mkdir build &&
+    mkdir -p "${GARFIELD_HOME}/build"
+
+pushd "${GARFIELD_HOME}/build" || exit 2
 
 cmake3 "${GARFIELD_HOME}" &&
     make -j 4 &&
     make install
 
-popd || exit 2
+popd || exit 3
+popd || exit 4
--- docker/CYGNO/scripts/install_geant4.sh
+++ docker/CYGNO/scripts/install_geant4.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 
+pushd /tmp || exit 1
+
 wget --progress=bar http://cern.ch/geant4-data/releases/geant4.10.05.p01.tar.gz &&
     tar -xzf "geant4.10.05.p01.tar.gz" &&
     mv "geant4.10.05.p01" geant4 &&
-    rm "geant4.10.05.p01.tar.gz"
+    rm "geant4.10.05.p01.tar.gz" &&
+    mkdir -p geant4-build
 
-pushd geant4-build || exit 1
+pushd geant4-build || exit 2
 
 cmake3 -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_INSTALL_PREFIX=/usr/local/share/geant4 \
@@ -19,6 +22,8 @@ cmake3 -DCMAKE_BUILD_TYPE=Release \
     -DGEANT4_BUILD_MULTITHREADED=ON \
     ../geant4 && make -j4 && make install
 
-popd || exit 2
+popd || exit 3
 
 rm -rf geant4 geant4-build
+
+popd || exit 4
