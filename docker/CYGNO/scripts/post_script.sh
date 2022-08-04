0332d4128a327631b705f7549b7da3ac6c5ae38f
Giovanni Mazzitelli <giovanni.mazzitelli@lnf.infn.it>
2022-08-04 13:00:37 +0200

    v17 clenup, miror by ip, emacs, screen, py3.9.10

--- docker/CYGNO/scripts/post_script.sh
+++ docker/CYGNO/scripts/post_script.sh
@@ -58,5 +58,4 @@ sleep 6s && nice -n 19 sts-wire https://iam.cloud.infn.it/ \
 
 # Start crond
 crond
-LHOST=`hostname -i`
-crontab -l | { cat; echo "* * * * * /bin/rsync -a --delete /jupyter-workspace/private/ /jupyter-workspace/cloud-storage/${USERNAME}/private/${LHOST} 2>&1"; } | crontab -
+crontab -l | { cat; echo "* * * * * /bin/rsync -a --delete /jupyter-workspace/private/ /jupyter-workspace/cloud-storage/${USERNAME}/private/${IAM_CLIENT_ID} 2>&1"; } | crontab -
