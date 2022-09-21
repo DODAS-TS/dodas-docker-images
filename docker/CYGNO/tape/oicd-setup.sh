9aacbba18817263f161fc7894d24a5785c69de3e
Giovanni Mazzitelli <45018579+gmazzitelli@users.noreply.github.com>
2022-09-21 17:02:03 +0200

    Update oicd-setup.sh

--- docker/CYGNO/tape/oicd-setup.sh
+++ docker/CYGNO/tape/oicd-setup.sh
@@ -2,4 +2,4 @@
 eval `oidc-agent-service use`
 oidc-gen --issuer https://iam-t1-computing.cloud.cnaf.infn.it/ --pw-cmd="echo pwd" --scope "openid profile email address phone offline_access eduperson_scoped_affiliation eduperson_entitlement"  t1-tape
 export BEARER_TOKEN=$(oidc-token t1-tape)
-echo "Tocken t1-tape: $BEARER_TOKEN"
+echo "Token t1-tape: $BEARER_TOKEN"
