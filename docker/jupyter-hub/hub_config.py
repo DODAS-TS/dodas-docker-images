bd027def6b130d9b2ba05f4a7f00e0c64ae3f0e4
dciangot <dciangot@cern.ch>
2023-06-28 12:20:24 +0200

    Update hub_config.py
    dodasts/spark:v3.0.3 with latest sts-wire image

--- docker/jupyter-hub/hub_config.py
+++ docker/jupyter-hub/hub_config.py
@@ -181,7 +181,7 @@ if 'JUPYTERHUB_CRYPT_KEY' not in os.environ:
 
 c.JupyterHub.spawner_class = 'kubespawner.KubeSpawner'
 c.KubeSpawner.cmd = ['/opt/conda/bin/jupyterhub-singleuser', '--allow-root']
-c.KubeSpawner.image = 'dodasts/spark:v3.0.1-patch1'
+c.KubeSpawner.image = 'dodasts/spark:v3.0.3'
 
 # TODO: PUT ENV
 
